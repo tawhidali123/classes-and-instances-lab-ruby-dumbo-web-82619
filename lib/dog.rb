5017689b16911584bd6c2201ef2cbdef9943e7d9
Tawhid Ali <tawhidali126@gmail.com>
2019-08-28 12:36:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+  puts "who let the dogs out"
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  puts "i am human"
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
